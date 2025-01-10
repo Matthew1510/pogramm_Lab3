98452440e4eb6e8b807379d14f4abcd599549197
Matthew1510 <matveyscout@gmail.com>
2025-01-10 23:51:39 +0300

    Update Robin.java

--- classes/Robin.java
+++ classes/Robin.java
@@ -4,8 +4,10 @@ import SetEnum.*;
 import SetInterface.*;
 
 public class Robin extends Characters implements Dialog {
-    public Robin(String Name, Nature Nature, Situasion Situasion, Status Status) {
-        super(Name, Nature, Situasion, Status);
+    public Robin(String Name, Nature Nature, Situasion Situasion, Status Status, Nationality Nationality,int Height, int Weight) {
+        super(Name, Nature, Situasion, Status, Nationality);
+        this.Weight = Weight;
+        this.Height = Height;
     }
 
     @Override
@@ -18,14 +20,26 @@ public class Robin extends Characters implements Dialog {
         if (Words.equals("")){
             throw new CustomException("необходимо что-то сказать");
         }
-        System.out.print("Робинзон говорит " + Words);
+        System.out.print(this.Name+ " говорит " + Words);
     }
     @Override
     public void Answer(String Words)  throws CustomException{
         if (Words.equals(" ")){
             throw new CustomException("необходимо что-то ответить");
         }
-        System.out.print("Робинзон отвечает " + Words);
+        System.out.print(this.Name+ " отвечает " + Words);
     }
-
+    @Override
+    public String toString() {
+        return "Characters{" +
+                ", Name = '" + this.Name + '\'' +
+                "Nature = " + this.Nature +
+                ", Situasion = " + this.Situasion +
+                ", Status = " + this.Status +
+                ", Item = " + this.Item +
+                ", Alive = " + this.Alive +
+                ", Height = " + this.Height +
+                ", Weight = " + this.Weight +
+                '}';
+    }
 }
