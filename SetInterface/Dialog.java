ec154064e84798126977ad812b65238e098dd14b
Matthew1510 <matveyscout@gmail.com>
2025-01-11 07:42:07 +0300

    Delete SetInterface directory

--- SetInterface/Dialog.java
+++
@@ -1,7 +0,0 @@
-package SetInterface;
-
-import classes.CustomException;
-public interface Dialog {
-    public void Say(String Words) throws CustomException;
-    public void Answer(String Words) throws CustomException;
-}
